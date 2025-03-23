070036904325ada6a1679040206ff382d76ee568
carl0s351 <carl0st0rres@hotmail.com>
2025-03-23 15:13:20 -0500

    Update Calculo Horas Trabajadas.py
    se agregaron 2 variables con los valores de hora para persona administrativa he Ingeniero

--- Calculo Horas Trabajadas.py
+++ Calculo Horas Trabajadas.py
@@ -1,7 +1,9 @@
 #Calculo horas trabajadas
 #Preguntar al usuario el N° de horas trabajadas y mostrar en pantalla la paga que le corresponde
 
-valor_hora = 7.824
+valor_hora_obrero = 7.824
+valor_hora_admin = 10.000
+valor_hora_ing = 12.000
 
 while True:
 
